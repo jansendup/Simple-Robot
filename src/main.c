7753a8368a48b329d29571e058df4f56ce79066f
Jansen du Plessis <jansendup@gmail.com>
2011-09-15 20:12:19 +0200

    Minior changes.

--- src/main.c
+++ src/main.c
@@ -1,17 +1,28 @@
 #include "p30F4011.h"
 #include "system.h"
 #include "io_ports.h"
+#include "ultra_sound.h"
+#include "motor_control.h"
 
 void flash_led_test();
+void init();
+
 void _500ms_delay()
 {
     long i;
     for(i = 0; i < 7000000L; i++);
 }
 
-int main()
+void init()
 {
 	init_io_ports();
+	init_ultra_sound();
+	init_pwm();
+}
+
+int main()
+{
+	init();
 	flash_led_test();
 	return 0;
 }
