cd49d881c0f6bbe81f2e9f8b266dd82bc4e232ec
Jansen du Plessis <jansendup@gmail.com>
2011-09-15 17:59:17 +0200

    PWM Speed control

--- src/motor_control.h
+++ src/motor_control.h
@@ -4,6 +4,11 @@
 #define PWM_FREQ 20000
 #define PWM_PRESCALE 1
 #define PWM_PER ( FCY / (PWM_FREQ * PWM_PRESCALE) - 1 )
+#define PWM_SCALE (PWM_PER/100)
+
+#define LEFT_MOTOR 1
+#define RIGHT_MOTOR 2
+#define PWM_SET_SPEED(MOTOR, DUTY_CYCLE) PDC##MOTOR = DUTY_CYCLE*PWM_SCALE;
 
 void init_pwm(void);
 
