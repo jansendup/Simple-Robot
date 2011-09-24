ce8b3e70f620b0473cb2ab3ce77960d6f1c02b43
Jansen du Plessis <jansendup@gmail.com>
2011-09-24 20:52:45 +0200

    Added a main loop.

--- src/main.c
+++ src/main.c
@@ -3,9 +3,14 @@
 #include "io_ports.h"
 #include "ultra_sound.h"
 #include "motor_control.h"
+#include "ad_converter.h"
 
 void flash_led_test();
 void init();
+void process_analog_inputs();
+
+extern volatile int analog_data[AD_BUFFER_SIZE];
+extern volatile char new_analog_data;
 
 void _500ms_delay()
 {
@@ -16,15 +21,30 @@ void _500ms_delay()
 void init()
 {
 	init_io_ports();
-	init_ultra_sound();
+	init_ad_converter();
 	init_pwm();
+	init_ultra_sound();
 }
 
 int main()
 {
 	init();
 	flash_led_test();
+
+	/** System Loop **/
+	while(1)
+	{
+		if( new_analog_data )
+			process_analog_inputs();
+	}
+
 	return 0;
+}
+
+void process_analog_inputs()
+{
+	new_analog_data = 0;
+	/*TODO: Process analog input data.*/
 }
 
 void flash_led_test()
