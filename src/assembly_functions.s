505169b6cc9f997e2def984db1bf9fd652dcf29a
Jansen du Plessis <jansendup@gmail.com>
2011-09-28 20:49:38 +0200

    Wrote time delay functions

---
+++ src/assembly_functions.s
@@ -0,0 +1,26 @@
+.text
+.global _delay_us
+.global _delay_ms
+
+fosc    = 7372800
+pll     = 16
+fcy     = fosc*pll/4
+ms_cycles = fcy/2000
+us_cycles = fcy/1000000
+ 
+ _delay_us:
+         repeat  #us_cycles-4
+         nop
+         dec     w0,w0
+         bra     NZ,_delay_us
+         return
+
+_delay_ms:
+         repeat  #ms_cycles-1
+         nop
+         repeat  #ms_cycles-4
+         nop
+         dec     w0,w0
+         bra     NZ,_delay_us
+         return
+.end
