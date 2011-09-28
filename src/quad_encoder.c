e18eaf3149015b5089fcd66ac2ddc22731716708
Jansen du Plessis <jansendup@gmail.com>
2011-09-28 21:42:50 +0200

    Wrote initialize code, and started to work on interrupt routines for software implemented quad decoder.

--- src/quad_encoder.c
+++ src/quad_encoder.c
@@ -1,5 +1,44 @@
 #include "quad_encoder.h"
+#include "p30F4011.h"
+
+#define TICKS_PER_REV  36
+#define COUNT_CENTER   (4*TICKS_PER_REV)
+#define MAX_COUNT      (2*COUNT_CENTER)
+
+int poscnt2;
 
 void init_quad_encoder(void)
 {
+   _QEOUT = 1;   // Enable digital filter.
+   _QECK = 5;    // 1:64 Clock Divide
+
+   _QEIM = 7;     // x4 mode with position counter reset by match (MAXCNT)
+   _POSRES = 0;   // Index Pulse doesn't reset Posistion Counter
+   _TQCS = 0;     // Selects internal clock.
+   _UPDN_SRC = 1;   // QEB pin State Defines Position Counter Direction
+   
+   POSCNT = 0;  // Initialize count to zero.
+   MAXCNT = MAX_COUNT;
+   POSCNT1 = COUNT_CENTER;
+   POSCNT2 = COUNT_CENTER;
+
+   /** Initialize External Interrupt 1 & 2 phase A and B of second decoder **/
+    _INT1IF = 0;    // Clear External Interrupt 1 Flag Status bit
+    _INT1IE = 1;    // Enable External Interrupt 1
+    _INT1EP = 1;    // Interrupt on negative edge
+    _INT1IP = 2;    // Interupt priority
+    
+    _INT2IF = 0;    // Clear External Interrupt 2 Flag Status bit
+    _INT2IE = 1;    // Enable External Interrupt 2
+    _INT2EP = 1;    // Interrupt on negative edge
+    _INT2IP = 2;    // Interupt priority
+}
+void __attribute__((__interrupt__)) _INT1Interrupt(void)
+{
+	IFS0bits.INT0IF = 0;	// Clear External Interrupt 2 Flag Status bit
+}
+
+void __attribute__((__interrupt__)) _INT2Interrupt(void)
+{
+	IFS0bits.INT0IF = 0;	// Clear External Interrupt 2 Flag Status bit
 }
--- src/quad_encoder.h
+++ src/quad_encoder.h
@@ -1,6 +1,9 @@
 #ifndef QUAD_ENCODER
 #define QUAD_ENCODER
 
+#define POSCNT1 POSCNT
+#define POSCNT2 poscnt2
+
 void init_quad_encoder(void);
 
 #endif
