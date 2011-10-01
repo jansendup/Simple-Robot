08a0f0f6802b9f068834f75a5d5cb7abee53f7ed
Jansen du Plessis <jansendup@gmail.com>
2011-10-01 21:24:31 +0200

    Added std_lib files.

---
+++ src/std_lib.c
@@ -0,0 +1,6 @@
+#include "std_lib.h"
+
+void int_to_str(char* str, int num)
+{
+    // TODO: Implement
+}
--- src/std_lib.h
+++ src/std_lib.h
@@ -1,1 +1,6 @@
-#include "std_lib.h"
+#ifndef STD_LIB_H
+#define STD_LIB_H
+
+void int_to_str(char* str, int num);
+
+#endif
