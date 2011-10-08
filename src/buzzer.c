533c9c217ba8c6a18114d50f16b4d39dff166b7a
Jansen du Plessis <jansendup@gmail.com>
2011-10-09 00:14:30 +0200

    Buzzer

---
+++ src/buzzer.c
@@ -0,0 +1,39 @@
+#include "buzzer.h"
+#include "io_ports.h"
+#include "ultra_sound.h"
+#include "system.h"
+
+#define PROX_COUNT_FACTOR   (US_TIME_OUT_PERIOD/SYSTEM_TIMER_FRQ)
+#define PROX_BUZZ_TIME      100E-3
+#define PROX_BUZZ_COUNT     SYSTEM_TIMER_FRQ*PROX_BUZZ_TIME
+
+int counter = 0;
+volatile char buzz_proximity = 1;
+
+extern volatile char us_obj_detected;
+extern volatile int us_obj_distance;
+
+void update_buzzer()
+{
+    int cnt_threshold;
+    counter++;
+    
+    if(buzz_proximity)
+    {
+        if(counter > PROX_BUZZ_COUNT)
+        {
+            // Turn off buzzer
+            bit_clr_reg1(BUZZER);
+        }
+        if(us_obj_detected)
+        {
+            cnt_threshold = us_obj_distance/PROX_COUNT_FACTOR;
+            if(counter > cnt_threshold)
+            {
+                // Buzz the buzzer
+                bit_set_reg1(BUZZER);
+                counter = 0;
+            }
+        }
+    }
+}
---
+++ src/buzzer.h
@@ -0,0 +1,6 @@
+#ifndef BUZZER_H
+#define BUZZER_H
+
+void update_buzzer();
+
+#endif
